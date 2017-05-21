29c3f80429e384b08d7f38acb41a29e756640d37
Jventas <jesusventasmdl@gmail.com>
2017-05-21 03:42:22 +0200

    p52 intento57

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -209,7 +209,7 @@ exports.randomplay = function (req, res, next) {
             randomIndex++;
         }
         
-        idRandom = req.session.restantes[randomIndex];
+        idRandom = req.session.restantes.length;
 
 
         //var arrayRestantes = req.session.restantes.length === 0 ? [-1] : req.session.restantes;
