09e8a76464a50741b62d97fb3a611f5c4f11e2c6
Jventas <jesusventasmdl@gmail.com>
2017-05-21 04:40:36 +0200

    p52 intento76

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -197,9 +197,9 @@ exports.randomplay = function (req, res, next) {
         if(!req.session.restantes || req.session.restantes.length === 0){
             req.session.restantes = [];
             req.session.aciertos = 0;
-            var quizzes = models.Quiz.findAll();
+            var todos = models.Quiz.findAll();
             for(var i=0;i<count;i++){
-                req.session.restantes.push(quizzes[i].id); //Guardamos todos los ID - [1-count]
+                req.session.restantes.push(todos[i].id; //Guardamos todos los ID - [1-count]
             }
         }
 
