e20fbba81e7c28f22b8ed83e6bcf873346ad9393
Jventas <jesusventasmdl@gmail.com>
2017-06-05 16:30:11 +0200

    intento3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -82,7 +82,9 @@ exports.index = function (req, res, next) {
 
         findOptions.offset = items_per_page * (pageno - 1);
         findOptions.limit = items_per_page;
-        findOptions.include = [{model: models.User, as: 'Author'}];
+        findOptions.include = [
+            {model: models.Tip, include: [{model: models.User, as: 'Author'}]},
+            {model: models.User, as: 'Author'}];
 
         return models.Quiz.findAll(findOptions);
     })
